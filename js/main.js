b62ba4e3e4ebbd44a4f141abdc5584a4f93e9008
Nicolas Pinzon <np07qsf@gmail.com>
2025-09-03 14:56:34 -0500

    Creo la funcion de la estrella

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
+// funcion del circulo
 const circleBtn = document.getElementById('btnCircle');
 function circle() {
     const element = document.getElementById('figure');
     element.classList.toggle('circle')
 }
-c
+// funcion de la estrella
+
+const starBtn = document.getElementById('btnStar');
+function star() {
+    const element = document.getElementById('figure');
+    element.classList.toggle('star')
+
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     circleBtn.addEventListener('click', circle)
+    starBtn.addEventListener('click', star)
 })
