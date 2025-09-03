a49335830c91e293a0cc6ccc93299adfd10364de
Nicolas Pinzon <np07qsf@gmail.com>
2025-09-03 14:50:04 -0500

    Creo la funcion del circulo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const circleBtn = document.getElementById('btnCircle');
+function circle() {
+    const element = document.getElementById('figure');
+    element.classList.toggle('circle')
+}
+c
+document.addEventListener('DOMContentLoaded', function () {
+    circleBtn.addEventListener('click', circle)
+})
